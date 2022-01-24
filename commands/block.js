145deeb052e8bd0bcabe6714b88e634406778f9d
ajdj100 <ajairplane@gmail.com>
2022-01-24 09:45:57 -0500

    Added a fix to block.js which should catch issues caused by not being able to detect the user's lab, or the user not having the role

--- commands/block.js
+++ commands/block.js
@@ -17,9 +17,12 @@ module.exports = {
         } else if(interaction.member.roles.cache.has('884797917204975687')) {
             console.log('user is in lab 2');
             await interaction.reply({content: 'You are in lab 3.', ephemeral: true});
-        }else if(interaction.member.roles.cache.has('933440451892092948')) {
+        } else if(interaction.member.roles.cache.has('933440451892092948')) {
             console.log('user is in lab 3');
             await interaction.reply({content: 'You are in lab 3.', ephemeral: true});
+        } else {
+            console.log("Couldn't get user role");
+            await interaction.reply({content: "Error getting lab, you either dont have the role or something went wrong", ephemeral: true});
         }
     },
 };
